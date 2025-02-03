e614e73ea798782b272b0fa6ccc39d936368f23a
Rishi Kalra <rishikalra122005@gmail.com>
2025-02-03 14:46:40 +0530

    implemented "hello" (I think :( )

--- defs.h
+++ defs.h
@@ -187,5 +187,8 @@ void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
 
+// hello nigga ;)
+int             hello();
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
